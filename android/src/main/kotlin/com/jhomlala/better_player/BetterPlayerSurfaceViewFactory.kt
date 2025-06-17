3f4402ba9d82a153485052c4c6b736b27ab16412
sgkrim <62644334+sgkrim@users.noreply.github.com>
2025-06-17 16:02:13 +0300

    Update BetterPlayerSurfaceViewFactory.kt

--- android/src/main/kotlin/com/jhomlala/better_player/BetterPlayerSurfaceViewFactory.kt
+++ android/src/main/kotlin/com/jhomlala/better_player/BetterPlayerSurfaceViewFactory.kt
@@ -1,9 +1,24 @@
+package com.jhomlala.better_player
+
+import android.content.Context
+import android.util.SparseArray
+import com.google.android.exoplayer2.ExoPlayer
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+/** Реєструє PlatformView і віддає йому існуючий ExoPlayer за id. */
 class BetterPlayerSurfaceViewFactory(
-  private val playerMap: SparseArray<ExoPlayer>
+    private val players: SparseArray<ExoPlayer>
 ) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
-  override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
-      val player = playerMap[viewId]     // дістаємо ExoPlayer із мапи
-      return BetterPlayerSurfaceView(context, player)
-  }
+    override fun create(
+        context: Context,
+        viewId: Int,
+        args: Any?
+    ): PlatformView {
+        val player = players[viewId]
+        requireNotNull(player) { "ExoPlayer with id=$viewId not found" }
+        return BetterPlayerSurfaceView(context, player)
+    }
 }
